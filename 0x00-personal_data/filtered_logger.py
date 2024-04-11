79b11e94b5132f3a5685baff184e8ffb3075cb93
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-11 22:17:06 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,12 +18,31 @@ log message obfuscated:
     to perform the substitution with a single regex.
 """
 
+import logging
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ A function that returns the log message obfuscated."""
-    pattern = '|'.join(fields)
-    return re.sub(f'({pattern})=[^{separator}]*', f'\\1={redaction}', message)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log records"""
+        def filter_datum(fields: List[str], redaction: str,
+                         message: str, separator: str) -> str:
+            """ A function that returns the log message obfuscated."""
+            pattern = '|'.join(fields)
+            return re.sub(f'({pattern})=[^{separator}]*',
+                          f'\\1={redaction}', message)
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
