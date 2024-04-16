c75e5d55225c378e360c88810e0c0eddb30dcfbd
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-16 21:06:30 +0000

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class for basic authentication."""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str | None:
+        """ Extract the Base64 part of the Authorization
+            header for Basic Authentication.
+        """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        # Extract the Base64 part after 'Basic ' which is 6 chars long
+        base64_part = authorization_header.split('Basic ')[1]
+        return base64_part if base64_part.strip() else None
