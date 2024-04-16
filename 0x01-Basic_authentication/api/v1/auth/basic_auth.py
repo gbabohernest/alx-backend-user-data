d6c0f0dfcf4c2069447577656dc0e393e5ebb224
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-16 22:27:09 +0000

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,3 +44,19 @@ class BasicAuth(Auth):
         except (base64.binascii.Error, UnicodeDecodeError):
             pass
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ Extract the user email and password from
+            the decoded Base64 authorization header.
+        """
+        if decoded_base64_authorization_header is None or not \
+                isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header \
+            .split(':')
+        return user_email, user_password
