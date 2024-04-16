39853dee03f0417f0fc1e86c2db8346f6aeb41e3
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-16 13:41:08 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,11 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 
-if AUTH_TYPE == 'auth':
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+else:
     from api.v1.auth.auth import Auth as AuthType
 
     auth = AuthType()
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines an BasicAuth class that inherits from Auth class.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class for basic authentication."""
+    pass
