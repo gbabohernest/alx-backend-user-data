dc3c22aab8b744bf629519d51f694410b2d7c198
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-16 21:37:20 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ This module defines an BasicAuth class that inherits from Auth class.
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,22 @@ class BasicAuth(Auth):
         # Extract the Base64 part after 'Basic ' which is 6 chars long
         base64_part = authorization_header.split('Basic ')[1]
         return base64_part if base64_part.strip() else None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decode the Base64 authorization header
+             and return the decoded value.
+         """
+        if base64_authorization_header is None or not \
+                isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+
+        except (base64.binascii.Error, UnicodeDecodeError):
+            pass
+            return None
