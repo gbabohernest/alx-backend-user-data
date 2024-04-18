889a6a9c56dbaa07f5b5711fb9daf62b71725c11
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-19 02:02:25 +0300

    User ID for Sesssion ID: Retrieve a User ID based on a Session ID.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,6 +14,7 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         """
         Create a Session ID for a user_id.
+
         Args:
             user_id (str): The user ID for which to create a session.
         Returns:
@@ -26,3 +27,19 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session(self, session_id: str = None) -> str:
+        """
+        Retrieve a User ID based on a Session ID.
+
+        Args:
+            session_id (str): The session ID for which to retrieve
+            the user ID.
+        Returns:
+            str: The user ID associated with the session ID if
+            found, else None.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
