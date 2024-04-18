534bff65f562268b8ed9e60afcbdc8a0f2852599
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-19 01:31:53 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,27 @@
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Class for session-based authentication.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id.
+        Args:
+            user_id (str): The user ID for which to create a session.
+        Returns:
+            str: The session ID if successful, None otherwise.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+
