fa51adefb8c7df45702786cffdf5e4fa512edcff
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-19 20:31:13 +0300

    fix correct output error, add remaining code for task 0 . fix inputerror

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,3 +47,34 @@ def before_request_handler():
         request.current_user = auth.current_user(request)
         if request.current_user is None:
             abort(403)
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler
+    """
+    response = jsonify({"error": "Forbidden"})
+    response.status_code = 403
+    return response
+
+
+@app.errorhandler(401)
+def not_authorized(error) -> str:
+    """ Unauthorized handler
+    """
+    response = jsonify({"error": "Unauthorized"})
+    response.status_code = 401
+    return response
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
