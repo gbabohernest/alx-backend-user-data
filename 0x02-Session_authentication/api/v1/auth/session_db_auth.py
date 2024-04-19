296be0f8a0803b71e8a124d60b48d0e7fc7f02a4
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-19 07:00:53 +0300

    handle case of an expired Session ID and return None accordinly

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -3,8 +3,9 @@
 """
 
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.app import auth
 from models.user_session import UserSession
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import TypeVar, Optional
 
 
@@ -47,7 +48,21 @@ class SessionDBAuth(SessionExpAuth):
         if not user_sessions:
             return None
 
-        return user_sessions[0].user_id
+        # Check if the session has expired
+        session_data = user_sessions[0]
+        created_at = datetime.strptime(session_data.created_at,
+                                       "%Y-%m-%dT%H:%M:%S")
+        session_duration = auth.session_duration
+        if session_duration > 0:
+            session_expiry = created_at + timedelta(seconds=session_duration)
+            if datetime.utcnow() > session_expiry:
+                # Expired session, remove from database and return None
+                session_data.remove()
+                return None
+
+        return session_data.user_id
+
+        # return user_sessions[0].user_id
 
     def destroy_session(self, request=None) -> bool:
         """ Destroy the UserSession based on Session ID
