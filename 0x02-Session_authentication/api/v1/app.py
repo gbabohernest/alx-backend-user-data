2d7a00a524b32e14127c90654788a176d4ae8a55
gbabohernest <gbaboh.ernest@gmail.com>
2024-04-19 03:04:16 +0300

    update @app.before_request, add a new URL path to the list of excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,11 +42,14 @@ def before_request_handler():
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      'api/v1/auth_session/login/'
+                      ]
 
     if auth.require_auth(request.path, excluded_paths):
         auth_header = auth.authorization_header(request)
-        if auth_header is None:
+        auth_cookie = auth.session_cookie(request)
+        if auth_header is None and auth_cookie is None:
             abort(401)
 
         request.current_user = auth.current_user(request)
