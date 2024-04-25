631a59adc6acf356e4709f963465afd754e2d7f1
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 10:26:09 +0000

    User model, SQLAlchemy model representing a User in the database.

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Defines a SQLAlchemy model representing a User in the database.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    SQLAlchemy model representing a User in the database.
+
+    Attributes:
+        id (int): The integer primary key.
+        email (str): A non-nullable string representing the user's email
+        hashed_password (str): A non-nullable string representing the hashed
+                               password
+        session_id (str): A nullable string representing the user's session ID.
+        reset_token (str): A nullable string representing the user's reset
+                           token.
+    """
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False, unique=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
