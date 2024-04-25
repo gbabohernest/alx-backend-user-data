6b123483e3316782bd1183fb574f080cc72ea088
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 20:15:26 +0000

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,5 +44,36 @@ def users() -> str:
         return jsonify(response), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    Route handler for the POST /sessions endpoint
+    to authenticate a user.
+
+    Returns:
+        JSON: A JSON response payload indicating success
+              of authentication. otherwise a 401 HTTP status.
+    """
+
+    from flask import request, make_response, abort
+    from auth import Auth
+    AUTH = Auth()
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+
+    # create a new session for the user
+    session_id = AUTH.create_session(email)
+
+    # set the session ID as a cookie in the response
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
