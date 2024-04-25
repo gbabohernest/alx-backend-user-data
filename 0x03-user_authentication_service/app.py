94435aebc31fe80fff224e9f4018c80eaaa3dfd1
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 14:35:23 +0000

    A basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines a Basic Flask application"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """
+    Route handler for the root endpoint ("/")
+
+    Returns:
+        JSON: A JSON response with a welcome message
+    """
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
