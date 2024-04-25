bee92a8f06c3fba635b9ce885c3541b7d584cb04
Ernest Push Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 17:34:55 +0000

    register a new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,5 +18,35 @@ def index() -> str:
     return jsonify(message)
 
 
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """
+    Route handler for the POST /users endpoint
+    to register a new user.
+
+    Returns:
+        JSON: A JSON response indicating success or failure
+              of user registration.
+    """
+    from flask import request
+    from auth import Auth
+    AUTH = Auth()
+
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email or not password:
+            return
+
+        user_created = AUTH.register_user(email, password)
+
+        if user_created:
+            return jsonify({"email": email, "message": "user created"})
+
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
