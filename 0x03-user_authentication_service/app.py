6561321e17d142a3c7fbadf961fa381c5a182d8b
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 18:38:01 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """Defines a Basic Flask application"""
 
-from flask import Flask, jsonify, request
-from auth import Auth
-
-AUTH = Auth()
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -20,64 +17,5 @@ def index() -> str:
     return jsonify(message)
 
 
-@app.route("/users", methods=["POST"]
-def users() -> str:
-    """
-    Route handler for the POST /users endpoint
-    to register a new user.
-
-    Returns:
-        JSON: A JSON response indicating success or failure
-              of user registration.
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
-
-    try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
-    except Exception:
-        return jsonify({"message": "email already registered"}), 400
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
-# @app.route("/", methods=["GET"], strict_slashes=False)
-# def index() -> str:
-#     """
-#     Route handler for the root endpoint ("/")
-
-#     Returns:
-#         JSON: A JSON response with a welcome message
-#     """
-#     message = {"message": "Bienvenue"}
-#     return jsonify(message)
-
-
-# @app.route("/users", methods=["POST"], strict_slashes=False)
-# def users() -> str:
-#     """
-#     Route handler for the POST /users endpoint
-#     to register a new user.
-
-#     Returns:
-#         JSON: A JSON response indicating success or failure
-#               of user registration.
-#     """
-#     email = request.form.get("email")
-#     password = request.form.get("password")
-#     try:
-#        # email = request.form.get("email")
-#        # password = request.form.get("password")
-
-#         user = AUTH.register_user(email, password)
-#         response = {"email": user.email, "message": "user created"}
-#         return jsonify(response)
-
-#     except ValueError:
-#         response = {"message": "email already registered"}
-#         return jsonify(response), 400
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port="5000")
