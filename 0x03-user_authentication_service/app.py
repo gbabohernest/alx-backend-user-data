8098da181b6664d0945a70e6f331ee9c1022203a
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-04-25 18:35:00 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,20 +20,27 @@ def index() -> str:
     return jsonify(message)
 
 
+@app.route("/users", methods=["POST"]
+def users() -> str:
+    """
+    Route handler for the POST /users endpoint
+    to register a new user.
+
+    Returns:
+        JSON: A JSON response indicating success or failure
+              of user registration.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
-
-# #!/usr/bin/env python3
-# """Defines a Basic Flask application"""
-
-# from flask import Flask, jsonify, request
-
-# from auth import Auth
-
-# app = Flask(__name__)
-# AUTH = AUTH()
-
 
 # @app.route("/", methods=["GET"], strict_slashes=False)
 # def index() -> str:
